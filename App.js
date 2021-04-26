ce1af4716e6c51ef99c097ee4f0b5cef93b80137
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2021-04-26 17:46:42 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [outputText, setOutputText] = useState(+0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button title="change" onPress={() => {setOutputText(outputText + 1)}}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    color: 'red',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
